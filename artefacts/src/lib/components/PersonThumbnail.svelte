a1183071ce4a9d34e6cbf152d41764e57b588b53
Delilah Chadic <delilahchadic@gmail.com>
2025-05-11 01:41:43 -0700

    refactor and remove errors raised in person thumbnail component

--- artefacts/src/lib/components/PersonThumbnail.svelte
+++ artefacts/src/lib/components/PersonThumbnail.svelte
@@ -9,13 +9,13 @@
   onMount(async () =>{
     const imageResponse = await fetch('http://localhost:8000/person/' + prop.person.id + '/image');
     const jsonImage = await imageResponse.json();
-    imageUrl = jsonImage.value;
-    fetchWikipediaFirstParagraphHTML(person.name)
+    // imageUrl = jsonImage.value;
+    imageUrl = await getWikiImage(person.name);
   });
 
-  async function fetchWikipediaFirstParagraphHTML(title: string): Promise<string> {
+  async function getWikiImage(title: string): Promise<string> {
     const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${encodeURIComponent(title)}&format=json&prop=text&origin=*`;
-
+    
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
@@ -24,29 +24,14 @@
         const htmlContent = data.parse.text['*'];
         const tempDiv = document.createElement('div');
         tempDiv.innerHTML = htmlContent;
-        
-        // const firstParagraph = tempDiv.querySelector('.mw-parser-output > p:not(.mw-empty-elt)');
         const images = tempDiv.querySelectorAll('img');
-        // console.log(images)
-        imageUrl = images[0].src;
-        // const links = firstParagraph?.querySelectorAll('a');
-        
-
-        // links.forEach(link => {
-        //   const href = link.getAttribute('href');
-        //   if (href && href.startsWith('/wiki/')) {
-        //     link.setAttribute('href', `https://en.wikipedia.org${href}`);
-        //     link.setAttribute('target', '_blank'); // Optional: Open links in a new tab
-        //     link.setAttribute('rel', 'noopener noreferrer'); // Recommended for security when using target="_blank"
-        //   }
-        // });
-        return firstParagraph ? firstParagraph.outerHTML : "";
+        let url = images[0]? images[0].src : "";
+        return url;
       } else {
-        console.error("Could not extract HTML from Wikipedia API response.");
         return "";
       }
     } catch (error) {
-      console.error("Error fetching from Wikipedia API:", error);
+      console.error("Error fetching from Wikipedia API:", error , title);
       return "";
     }
 }
