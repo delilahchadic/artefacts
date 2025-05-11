7f786dd2f0ddea3d0e02065d15b84f1ab5bef1d9
Delilah Chadic <delilahchadic@gmail.com>
2025-05-11 00:59:37 -0700

    implement simple light box on work by pk route

--- artefacts/src/routes/work/[pk]/+page.svelte
+++ artefacts/src/routes/work/[pk]/+page.svelte
@@ -4,10 +4,9 @@
   import {IIIFParser} from "$lib/parse.js";
  
 
+  let lightbox =$state(false);
 
 	let prop: PageProps = $props();
-  let data = $state([]);
-  let works = $state([]);
   let iiif = $state(null);
   let creators = $state([]);
   let label = $state("");
@@ -16,15 +15,14 @@
   let images = $state([]);
   let iiif_link = $state("");
   let description = $state("");
+  let activeImage = $state("");
   interface Work {
     id: number;
-    name: string; // Add the 'name' property
-    // Add other properties of your Work interface
+    name: string;
   }
 
   interface IIIFResponse {
     link: string;
-    // Add other properties of your IIIF response
   }
   let test = $state<IIIFParser |null>(null);
   onMount(async () => {
@@ -46,14 +44,39 @@
     iiif = manifestJson
     description = test.getDescription();
   });
+
+  function clickImage(imageUrl){
+    lightbox = true;
+    setActiveImage(images[0]);
+  }
+
+  function setActiveImage(imageUrl){
+    activeImage=imageUrl;
+    console.log(imageUrl);
+  }
   </script>
 
+{#if lightbox}
+<div class="h-[100vh] w-[100vh]">
+  <div class="relative max-h-[80%] flex  content-center">
+    
+    <img class="max-h-[70vh] mx-auto" src={activeImage}/>
+    <button class="absolute top-0 right-2 bg-gray-200 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded button" onclick="{() => lightbox=false}">X</button>
+</div>
+<div class="grid grid-cols-6 max-h-[10%]">
+{#each images as image}
+  <img class="p-4" src={image} onclick="{() => setActiveImage(image)}"/>
+{/each}
+</div>
+</div>
+  
+{/if}
 <div class="">
   <h1 class="text-3xl text-center mb-10">{label}</h1>
   <div class="flex flex-row gap-10" >
 
     <div class="mr-4 max-w-[30vw] w-fit flex-shrink-0 justify-self-center">
-      <div class="w-fit"><img class= "pb-10 h-auto max-h-[70vh] w-auto object-contain ml-5" src={imageurl}/></div>
+      <div class="w-fit"><img class= "pb-10 h-auto max-h-[70vh] w-auto object-contain ml-5" src={imageurl} onclick="{() =>clickImage(imageurl)}"/></div>
       
       <div class="break-words max-w-[20vw]" >
       {#each metadata as data }
@@ -77,7 +100,7 @@
   <div class="grid max-h-[20vh] grid-cols-5">
     {#each images as image}
     <div class=" p-5 m-5 shadow-2xl">
-      <img src={image}/>
+      <img src={image} onclick="{() =>clickImage(image)}"/>
     </div>
     
     {/each}
